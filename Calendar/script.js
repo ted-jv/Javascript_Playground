3237c830ad4a4b38f94a219b17e24a6f60489979
ted-jv <wkejrdldls@gmail.com>
2022-11-28 22:15:16 +0900

    Feat: 추가 - prev, next 아이콘 클릭시 달력 rerender 기능
    html - id 추가 & 각 btn 리스너 추가 & renderCalendear 내용 추가

--- Calendar/script.js
+++ Calendar/script.js
@@ -1,5 +1,6 @@
 const currentDate = document.querySelector('.current-date');
 const daysTag = document.querySelector('.days');
+const prevNextBtn = document.querySelectorAll('.icons span');
 
 // new Date
 let date = new Date(),
@@ -9,8 +10,16 @@ let date = new Date(),
 const months = ['January', 'February', 'March', 'April', 'May', 'August', 'Jun', 'July', 'September', 'October', 'November', 'December'];
 
 const renderCalendar = () => {
+  let firstDayofMonth = new Date(currYear, currMonth, 1).getDay();
   let lastDayOfMonth = new Date(currYear, currMonth + 1, 0).getDate();
+  lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+
   let liTag = '';
+
+  for (let i = firstDayofMonth; i > 0; i--) {
+    liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+  }
+
   for (let i = 1; i <= lastDayOfMonth; i++) {
     liTag += `<li>${i}</li>`;
   }
@@ -18,4 +27,12 @@ const renderCalendar = () => {
   currentDate.innerText = `${months[currMonth]} ${currYear}`;
   daysTag.innerHTML = liTag;
 };
+
+prevNextBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    currMonth = btn.id === 'prev' ? currMonth - 1 : currMonth + 1;
+    renderCalendar();
+  });
+});
+
 renderCalendar();
